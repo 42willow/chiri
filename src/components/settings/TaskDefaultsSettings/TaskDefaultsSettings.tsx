60edfc7ca74cb4b516fa4ed2ccb4bd293d00284d
willow <42willow@pm.me>
2026-07-01 00:38:05 +1000

    feat: update in process badges (#73)
    Co-authored-by: chloe 🐇 <chloe@sapphic.moe>

--- src/components/settings/TaskDefaultsSettings/TaskDefaultsSettings.tsx
+++ src/components/settings/TaskDefaultsSettings/TaskDefaultsSettings.tsx
@@ -1,10 +1,10 @@
 import Ban from 'lucide-react/icons/ban';
 import BellRing from 'lucide-react/icons/bell-ring';
 import Check from 'lucide-react/icons/check';
-import Loader from 'lucide-react/icons/loader';
 import Plus from 'lucide-react/icons/plus';
 import Repeat from 'lucide-react/icons/repeat';
 import RotateCcw from 'lucide-react/icons/rotate-ccw';
+import Timer from 'lucide-react/icons/timer';
 import X from 'lucide-react/icons/x';
 import { type CSSProperties, useState } from 'react';
 import { BatchTaskTagsModal } from '$components/modals/BatchTaskTagsModal';
@@ -178,7 +178,7 @@ export const TaskDefaultsSettings = () => {
                   {
                     value: 'in-process',
                     label: 'In Process',
-                    Icon: Loader,
+                    Icon: Timer,
                     iconClass: 'text-status-in-process',
                     activeClass:
                       'border-status-in-process bg-surface-200 dark:bg-surface-700 text-surface-900 dark:text-surface-100',
